d481e7a50d61cd32fc22f9ffa5fcd820bf17876b
henme <henrikkaald@gmail.com>
2016-01-21 00:57:00 +0100

    Update

--- Exercises/Ex02-Bottlenecks/Sol/helloworld_go.go
+++ Exercises/Ex02-Bottlenecks/Sol/helloworld_go.go
@@ -9,17 +9,27 @@ import (
     "time"
 )
 
+var i int
+
 func someGoroutine() {
-    Println("Hello from a goroutine!")
+    for x := 0; x < 100000; x++ {
+        i++
+    } 
+}
+
+func someGoroutine2() {
+    for x := 0; x < 100000; x++ {
+        i--
+    } 
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
                                             // Try doing the exercise both with and without it!
     go someGoroutine()                      // This spawns someGoroutine() as a goroutine
-
+    go someGoroutine2() 
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
-    Println("Hello from main!")
+    Println(i)
 }
