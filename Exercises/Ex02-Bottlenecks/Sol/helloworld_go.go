3ec02a00139516a8a4e0e1ab9053e663bafa29aa
henme <henrikkaald@gmail.com>
2016-01-21 17:18:58 +0100

    update

--- Exercises/Ex02-Bottlenecks/Sol/helloworld_go.go
+++ Exercises/Ex02-Bottlenecks/Sol/helloworld_go.go
@@ -6,35 +6,35 @@ package main
 import (
     "fmt"
     "runtime"
-    "time"
+    //"time"
     "sync"
 )
 
 var i int
 
 func main() {
-    message := make(chan int)
-    var wg sync.WaitGroup
+    //message := make(chan int) //Messages between processes
+    var wg sync.WaitGroup       //Synchronize a group of processes
     runtime.GOMAXPROCS(1)    // I guess this is a hint to what GOMAXPROCS does...
-    
+    // limit the ammount of threads to one, simple but slow implementation
     wg.Add(2)                                        // Try doing the exercise both with and without it!
     go func() {
         defer wg.Done()
         for x := 0; x < 100000; x++ {
             i++
         } 
-        message <- 1
+        //message <- 1
     }()                 // This spawns someGoroutine() as a goroutine
     go func() {
         defer wg.Done()
         for x := 0; x < 100000; x++ {
             i--
         } 
-        message <- 2
+        //message <- 2
     }() 
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     //time.Sleep(100*time.Millisecond)
     wg.Wait()
-    Println(i)
+    fmt.Printf("%v\n",i)
 }
